e88468c79cabc5244aea68ce2f5f0cdba7ea696f
Tama McGlinn <TGJ.McGlinn@mindef.nl>
2019-04-09 09:45:00 +0200

    updated adatags.sh

--- bash/adatags.sh
+++ bash/adatags.sh
@@ -1,5 +1,10 @@
-includeDirs=`find . -type d | grep -i 'source' | grep -vi 'test' | sed 's/^\.\//\-I/g' | paste -sd " " -`
-find . -type f -name '*.ali' | xargs gnat xref $includeDirs -vf | grep 'C:\|D:' | sort > tags
-cat tags >> ../tags
-sort ../tags | uniq > newglobaltags
-mv newglobaltags ../tags
+echo "!_TAG_FILE_SORTED 1 /0=unsorted, 1=sorted, 2=foldcase/" > tags
+find . -type d | grep -i '\./[a-z]*/source' \
+  | grep -vi 'test\|aonix\|64' \
+  | sed 's/^\.\//src_dir=/g' > gnat_xref_configuration.txt
+# find . -type f -name "*.ali" | grep -v 'CodeCAT' | sed 's/\/[^\/]*$//g' | sort | uniq \
+#   | sed 's/^\.\//obj_dir=/g' >> gnat_xref_configuration.txt
+find . -type f -name '*.ali' | xargs gnat xref -pgnat_xref_configuration.txt -vf \
+  | grep 'C:\|D:' \
+  | sort >> tags
+rm gnat_xref_configuration.txt
