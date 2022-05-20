6671d4371d941f9441a9ed571ba7e1961a5f0291
Tama McGlinn <t.mcglinn@gmail.com>
2022-05-20 09:06:42 +0200

    autoformat custom_lspconfig.lua

--- lua/custom_lspconfig.lua
+++ lua/custom_lspconfig.lua
@@ -1,13 +1,13 @@
 -- see https://github.com/mjlbach/lsp-containers/blob/master/clangd/init.lua
 require("nvim-lsp-installer").setup {}
 
---Incremental live completion
+-- Incremental live completion
 vim.o.inccommand = 'nosplit'
 
---Enable break indent
+-- Enable break indent
 vim.o.breakindent = true
 
---Case insensitive searching UNLESS /C or capital in search
+-- Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
@@ -16,100 +16,111 @@ vim.g.indent_blankline_char = ' '
 
 -- Telescope
 require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
+    defaults = {mappings = {i = {['<C-u>'] = false, ['<C-d>'] = false}}}
 }
 
 -- nvim-cmp setup
 local cmp = require('cmp')
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-u>'] = cmp.mapping.scroll_docs(4),
-    ['<C-b>'] = cmp.mapping.scroll_docs(-8),
-    ['<C-f>'] = cmp.mapping.scroll_docs(8),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+    snippet = {
+        expand = function(args)
+            vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end
     },
-    ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'ultisnips' },
-  }, {
-    { name = 'buffer' },
-  })
+    mapping = {
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(4),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-8),
+        ['<C-f>'] = cmp.mapping.scroll_docs(8),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+        },
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end
+    },
+    sources = cmp.config.sources({{name = 'nvim_lsp'}, {name = 'ultisnips'}},
+                                 {{name = 'buffer'}})
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
-})
+cmp.setup.cmdline('/', {sources = {{name = 'buffer'}}})
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+    sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
 })
 
 -- LSP settings
 
 local nvim_lsp = require 'lspconfig'
 local on_attach = function(_, bufnr)
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<CS-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>qo', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+    local opts = {noremap = true, silent = true}
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD',
+                                '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd',
+                                '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',
+                                '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi',
+                                '<cmd>lua vim.lsp.buf.implementation()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<CS-k>',
+                                '<cmd>lua vim.lsp.buf.signature_help()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa',
+                                '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr',
+                                '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl',
+                                '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D',
+                                '<cmd>lua vim.lsp.buf.type_definition()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn',
+                                '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr',
+                                '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca',
+                                '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca',
+                                '<cmd>lua vim.lsp.buf.range_code_action()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e',
+                                '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d',
+                                '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d',
+                                '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>qo',
+                                '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so',
+                                [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]],
+                                opts)
+    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -122,103 +133,109 @@ table.insert(runtime_path, "lua/?/init.lua")
 
 local lsp = require("lspconfig")
 
-local flake_ignores = { "E203", -- whitespace before :
-  "W503", -- line break before binary operator
-  "E501", -- line too long
-  "C901", -- mccabe complexity
-  "W293" } -- blank line contains whitespace
+local flake_ignores = {
+    "E203", -- whitespace before :
+    "W503", -- line break before binary operator
+    "E501", -- line too long
+    "C901", -- mccabe complexity
+    "W293"
+} -- blank line contains whitespace
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "pylsp", "rust_analyzer", "clangd", "bashls", "vimls", "sumneko_lua" }
+local servers = {
+    "pylsp", "rust_analyzer", "clangd", "bashls", "vimls", "sumneko_lua"
+}
 local settings = {
-  init_options = { documentFormatting = false },
-  pylsp = {
-    plugins = {
-      mccabe = { enabled = false },
-      pycodestyle = { enabled = false },
-      pydocstyle = { enabled = false },
-      pyflakes = { enabled = false },
-      mypy = { enabled = false },
-      pylint = { enabled = true },
-      yapf = { enabled = false },
-      rope = { enabled = false },
-      flake8 = {
-        enabled = true,
-        ignore = table.concat(flake_ignores, ",")
-      }
+    init_options = {documentFormatting = false},
+    pylsp = {
+        plugins = {
+            mccabe = {enabled = false},
+            pycodestyle = {enabled = false},
+            pydocstyle = {enabled = false},
+            pyflakes = {enabled = false},
+            mypy = {enabled = false},
+            pylint = {enabled = true},
+            yapf = {enabled = false},
+            rope = {enabled = false},
+            flake8 = {enabled = true, ignore = table.concat(flake_ignores, ",")}
+        }
+    },
+    Lua = {
+        runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = runtime_path
+        },
+        diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'}
+        },
+        workspace = {
+            checkThirdParty = false,
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true)
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {enable = false}
     }
-  },
-  Lua = {
-    runtime = {
-      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-      version = 'LuaJIT',
-      -- Setup your lua path
-      path = runtime_path,
-    },
-    diagnostics = {
-      -- Get the language server to recognize the `vim` global
-      globals = { 'vim' },
-    },
-    workspace = {
-      checkThirdParty = false,
-      -- Make the server aware of Neovim runtime files
-      library = vim.api.nvim_get_runtime_file("", true),
-    },
-    -- Do not send telemetry data containing a randomized but unique identifier
-    telemetry = {
-      enable = false,
-    },
-  },
 }
 
 lsp.als.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_init = require("gpr_selector").als_on_init,
-  single_file_support = true,
-  root_dir = function() return '/' end,
+    on_attach = on_attach,
+    capabilities = capabilities,
+    on_init = require("gpr_selector").als_on_init,
+    single_file_support = true,
+    root_dir = function() return '/' end
 }
 
 for _, lsp in pairs(servers) do
-  nvim_lsp[lsp].setup { on_attach = on_attach, capabilities = capabilities, settings = settings
-  }
+    nvim_lsp[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = settings
+    }
 end
-
 
 -- Treesitter configuration
 -- Parsers must be installed manually via :TSInstall
 require('nvim-treesitter.configs').setup {
-  highlight = {
-    enable = true, -- false will disable the whole extension
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
+    highlight = {
+        enable = true -- false will disable the whole extension
     },
-  },
-  indent = {
-    enable = true, },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = 'gnn',
+            node_incremental = 'grn',
+            scope_incremental = 'grc',
+            node_decremental = 'grm'
+        }
+    },
+    indent = {enable = true}
 }
 
-
 -- EFM setup
-require "lspconfig".efm.setup {
-  init_options = { documentFormatting = true },
-  settings = {
-    rootMarkers = { ".git/" },
-    languages = {
-      lua = {
-        { formatCommand = "lua-format -i", formatStdin = true },
-        { formatCommand = "lua-pretty -i" }
-      },
-      sh = {
-        { lintCommand = 'shellcheck -f gcc -x', lintSource = 'shellcheck', lintFormats = { '%f:%l:%c: %trror: %m', '%f:%l:%c: %tarning: %m', '%f:%l:%c: %tote: %m' } }
-      }
+require"lspconfig".efm.setup {
+    init_options = {documentFormatting = true},
+    settings = {
+        rootMarkers = {".git/"},
+        languages = {
+            lua = {
+                {formatCommand = "lua-format -i", formatStdin = true},
+                {formatCommand = "lua-pretty -i"}
+            },
+            sh = {
+                {
+                    lintCommand = 'shellcheck -f gcc -x',
+                    lintSource = 'shellcheck',
+                    lintFormats = {
+                        '%f:%l:%c: %trror: %m', '%f:%l:%c: %tarning: %m',
+                        '%f:%l:%c: %tote: %m'
+                    }
+                }
+            }
+        }
     }
-  }
 }
