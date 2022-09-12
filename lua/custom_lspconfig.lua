8bc79df2c467cfc5639586a994e94d7b4a9318f8
Tama McGlinn <t.mcglinn@gmail.com>
2022-09-12 15:57:58 +0200

    fix some things

--- lua/custom_lspconfig.lua
+++ lua/custom_lspconfig.lua
@@ -68,52 +68,24 @@ cmp.setup.cmdline(':', {
 local nvim_lsp = require 'lspconfig'
 local on_attach = function(_, bufnr)
     local opts = {noremap = true, silent = true}
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD',
-                                '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd',
-                                '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',
-                                '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi',
-                                '<cmd>lua vim.lsp.buf.implementation()<CR>',
-                                opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<CS-k>',
-                                '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-                                opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa',
-                                '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
-                                opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr',
-                                '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
-                                opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl',
-                                '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-                                opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D',
-                                '<cmd>lua vim.lsp.buf.type_definition()<CR>',
-                                opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn',
-                                '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr',
-                                '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca',
-                                '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca',
-                                '<cmd>lua vim.lsp.buf.range_code_action()<CR>',
-                                opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e',
-                                '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-                                opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d',
-                                '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d',
-                                '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>qo',
-                                '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
-                                opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so',
-                                [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]],
-                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<CS-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>qo', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
     vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
@@ -125,7 +97,8 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-local lsp = require("lspconfig")
+local lsp = require('lspconfig')
+local util = require('lspconfig.util')
 
 local flake_ignores = {
     "E203", -- whitespace before :
@@ -136,25 +109,43 @@ local flake_ignores = {
     "E402" -- module import not at top (covered by pylint instead)
 }
 
+lsp.als.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  on_init = require("gpr_selector").als_on_init,
+  single_file_support = true,
+  root_dir = function() return '/' end
+}
+lsp.pylsp.setup {
+  cmd = {"pylsp"},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  root_dir = function(fname)
+    return util.find_git_ancestor(fname)
+  end,
+  settings = {
+    pylsp = {
+      configurationSources = {"pylint"},
+      plugins = {
+        mccabe = {enabled = false},
+        pycodestyle = {enabled = false},
+        pydocstyle = {enabled = false},
+        pyflakes = {enabled = false},
+        mypy = {enabled = false},
+        pylint = {enabled = true},
+        yapf = {enabled = false},
+        rope = {enabled = false},
+        flake8 = {enabled = true, ignore = table.concat(flake_ignores, ",")}
+      }
+    }
+  }
+}
+
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = {"pylsp", "rust_analyzer", "bashls", "vimls", "sumneko_lua"}
+local default_servers = {"rust_analyzer", "bashls", "vimls", "sumneko_lua"}
 local settings = {
     init_options = {documentFormatting = false},
-    pylsp = {
-        root_dir = nvim_lsp.util.root_pattern('.git'),
-        plugins = {
-            mccabe = {enabled = false},
-            pycodestyle = {enabled = false},
-            pydocstyle = {enabled = false},
-            pyflakes = {enabled = false},
-            mypy = {enabled = false},
-            pylint = {enabled = true},
-            yapf = {enabled = false},
-            rope = {enabled = false},
-            flake8 = {enabled = true, ignore = table.concat(flake_ignores, ",")}
-        }
-    },
     Lua = {
         runtime = {
             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
@@ -176,16 +167,9 @@ local settings = {
     }
 }
 
-lsp.als.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    on_init = require("gpr_selector").als_on_init,
-    single_file_support = true,
-    root_dir = function() return '/' end
-}
 
-for _, lsp in pairs(servers) do
-    nvim_lsp[lsp].setup {
+for _, servername in pairs(default_servers) do
+    nvim_lsp[servername].setup {
         on_attach = on_attach,
         capabilities = capabilities,
         settings = settings,
--- remotesettings.vim
+++ remotesettings.vim
@@ -1,4 +1,5 @@
 
+let $EDITOR = 'nvr --nostart'
 let $GIT_EDITOR = 'nvr -cc split --remote-wait'
 
 " When editing for git, delete hidden buffers, so that :wq will signal to nvr to stop waiting, so that git can continue
