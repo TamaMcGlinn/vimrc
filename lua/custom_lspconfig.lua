7fcf935401d0a1ae23f4cd206072b91246e6c821
Tama McGlinn <t.mcglinn@gmail.com>
2022-10-14 14:15:34 +0200

    reformat lspconfig

--- lua/custom_lspconfig.lua
+++ lua/custom_lspconfig.lua
@@ -68,24 +68,52 @@ cmp.setup.cmdline(':', {
 local nvim_lsp = require 'lspconfig'
 local on_attach = function(_, bufnr)
     local opts = {noremap = true, silent = true}
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<CS-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>qo', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD',
+                                '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd',
+                                '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',
+                                '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi',
+                                '<cmd>lua vim.lsp.buf.implementation()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<CS-k>',
+                                '<cmd>lua vim.lsp.buf.signature_help()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa',
+                                '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr',
+                                '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl',
+                                '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D',
+                                '<cmd>lua vim.lsp.buf.type_definition()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn',
+                                '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr',
+                                '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca',
+                                '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca',
+                                '<cmd>lua vim.lsp.buf.range_code_action()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e',
+                                '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d',
+                                '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d',
+                                '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>qo',
+                                '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>',
+                                opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so',
+                                [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]],
+                                opts)
     vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
@@ -110,40 +138,43 @@ local flake_ignores = {
 }
 
 lsp.als.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  on_init = require("gpr_selector").als_on_init,
-  single_file_support = true,
-  root_dir = function() return '/' end
+    on_attach = on_attach,
+    capabilities = capabilities,
+    on_init = require("gpr_selector").als_on_init,
+    single_file_support = true,
+    root_dir = function() return '/' end
 }
 lsp.pylsp.setup {
-  cmd = {"pylsp"},
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = function(fname)
-    return util.find_git_ancestor(fname)
-  end,
-  settings = {
-    pylsp = {
-      configurationSources = {"pylint"},
-      plugins = {
-        mccabe = {enabled = false},
-        pycodestyle = {enabled = false},
-        pydocstyle = {enabled = true},
-        pyflakes = {enabled = false},
-        mypy = {enabled = false},
-        pylint = {enabled = true},
-        yapf = {enabled = false},
-        rope = {enabled = false},
-        flake8 = {enabled = true, ignore = table.concat(flake_ignores, ",")}
-      }
+    cmd = {"pylsp"},
+    on_attach = on_attach,
+    capabilities = capabilities,
+    root_dir = function(fname) return util.find_git_ancestor(fname) end,
+    settings = {
+        pylsp = {
+            configurationSources = {"pylint"},
+            plugins = {
+                mccabe = {enabled = false},
+                pycodestyle = {enabled = false},
+                pydocstyle = {enabled = true},
+                pyflakes = {enabled = false},
+                mypy = {enabled = false},
+                pylint = {enabled = true},
+                yapf = {enabled = false},
+                rope = {enabled = false},
+                flake8 = {
+                    enabled = true,
+                    ignore = table.concat(flake_ignores, ",")
+                }
+            }
+        }
     }
-  }
 }
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local default_servers = {"clangd", "rust_analyzer", "bashls", "vimls", "sumneko_lua"}
+local default_servers = {
+    "clangd", "rust_analyzer", "bashls", "vimls", "sumneko_lua"
+}
 local settings = {
     init_options = {documentFormatting = false},
     Lua = {
@@ -166,7 +197,6 @@ local settings = {
         telemetry = {enable = false}
     }
 }
-
 
 for _, servername in pairs(default_servers) do
     nvim_lsp[servername].setup {
