5a90408babf961c44bfb0f5c10969e32a04470ea
Tama McGlinn <t.mcglinn@gmail.com>
2024-03-01 15:19:58 +0100

    cleanup codegpt maps

--- lua/chatgpt_config.lua
+++ lua/chatgpt_config.lua
@@ -5,31 +5,13 @@ require'nvim-gpt'.setup {
     -- see more setup options in section 'Setup options' below
 }
 
-vim.api.nvim_set_keymap('n', '<leader>ce', ':Chat completion ', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ca', ':Chat question ', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>ce', "'<,'>:Chat code_edit ", { noremap = true, silent = true })
-
-
--- when trying to use this in a terminal window, I got this error:
-
--- Error executing vim.schedule lua callback: /home/tama/.vim/plugged/CodeGPT.nvim/lua/codegpt/utils.lua:52: Buffer is not 'modifiable'                                                                        
--- stack traceback:
---         [C]: in function 'nvim_buf_set_text'
---         /home/tama/.vim/plugged/CodeGPT.nvim/lua/codegpt/utils.lua:52: in function 'replace_lines'
---         .../.vim/plugged/CodeGPT.nvim/lua/codegpt/commands_list.lua:29: in function 'callback'
---         .../tama/.vim/plugged/CodeGPT.nvim/lua/codegpt/commands.lua:21: in function 'cb'
---         ...im/plugged/CodeGPT.nvim/lua/codegpt/providers/openai.lua:85: in function 'handle_response'
---         ...ama/.vim/plugged/CodeGPT.nvim/lua/codegpt/openai_api.lua:55: in function ''
---         vim/_editor.lua: in function <vim/_editor.lua:0>
---
--- now I'm trying to create my own callback that works for the terminal
-
--- note I can also use {{text_selection}}
-
--- require("codegpt.config")
-
--- vim.g["codegpt_commands"] = {
--- }
+vim.api.nvim_command([[
+  nnoremap <Leader>ce :Chat completion 
+  vnoremap <Leader>ce :Chat code_edit 
+  nnoremap <Leader>ca :Chat question 
+  vnoremap <Leader>ca :Chat question 
+]])
+-- TODO this one doesn't work, for visual selection and then asking about it: vnoremap <Leader>ca :Chat question 
 
 -- uncomment the following for debugging CodeGPT calls to ChatGPT
 -- OpenAIApi = require("codegpt.openai_api")
