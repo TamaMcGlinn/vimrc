54f0e945983f0149a75838015d9e14dbe1120675
Tama McGlinn <t.mcglinn@gmail.com>
2024-08-07 10:20:42 +0000

    update noice maps

--- lua/noice_config.lua
+++ lua/noice_config.lua
@@ -16,3 +16,28 @@ require("noice").setup({
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
 })
+
+
+vim.api.nvim_call_function("floggit#update_whichkey", {'n', {
+  ['name'] = '+Noice',
+  ['l'] = 'Noice last',
+  ['h'] = 'Noice history',
+  ['E'] = 'Noice errors',
+  ['p'] = 'Noice pick',
+}})
+
+vim.keymap.set("n", "<leader>nl", function()
+  require("noice").cmd("last")
+end)
+
+vim.keymap.set("n", "<leader>nh", function()
+  require("noice").cmd("history")
+end)
+
+vim.keymap.set("n", "<leader>nE", function()
+  require("noice").cmd("errors")
+end)
+
+vim.keymap.set("n", "<leader>np", function()
+  require("noice").cmd("pick")
+end)
